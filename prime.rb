89bd6aaac86f8b217e6c6df0e3fc5f9631719be5
Patrick J Louis <lumenfuzz@gmail.com>
2018-03-14 17:22:58 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -3,4 +3,12 @@ def prime?(the_number)
 
   return true if the_number == 2 || the_number == 3
   return false if the_number % 2 == 0 || the_number % 3 == 0 || the_number < 0
+
+  divisor = 5
+  until divisor * divisor > the_number
+    return false if the_number % divisor == 0
+    # next prime is 6k + 1 or 6k - 1
+    if (divisor + 1) % 6 == 0 ? divisor += 2 : divisor += 4
+    end
+  end
 end
