b54cb855688d6134cafa98bce28469e74947d8a2
Patrick J Louis <lumenfuzz@gmail.com>
2018-03-14 17:18:04 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -2,5 +2,5 @@
 def prime?(the_number)
 
   return true if the_number == 2 || the_number == 3
-  false if the_number % 2 == 0 || the_number % 3 == 0 || the_number < 0
+  return false if the_number % 2 == 0 || the_number % 3 == 0 || the_number < 0
 end
