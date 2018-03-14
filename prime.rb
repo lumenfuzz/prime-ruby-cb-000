6986d0127bd7fff03cbcc8a5c56de328dbeb9b80
Patrick J Louis <lumenfuzz@gmail.com>
2018-03-14 17:09:11 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -9,6 +9,7 @@ def prime?(the_number)
     false if the_number % divisor == 0
     # next prime is 6k + 1 or 6k - 1
     if (divisor + 1) % 6 == 0 ? divisor += 2 : divisor += 4
+    end
   end
   true
 end
