bf3cc7562b66c6794914b117cb1d1c009a47b644
Patrick J Louis <lumenfuzz@gmail.com>
2018-03-14 17:10:10 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -3,7 +3,6 @@ def prime?(the_number)
 
   true if the_number == 2 || the_number == 3
   false if the_number % 2 == 0 || the_number % 3 == 0
-  false if the_number < 0
 
   divisor = 5
   until divisor * divisor > the_number
@@ -12,5 +11,6 @@ def prime?(the_number)
     if (divisor + 1) % 6 == 0 ? divisor += 2 : divisor += 4
     end
   end
+  false if the_number < 0
   true
 end
